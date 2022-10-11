d2178b49ea189426d8533202ffe4db608ce7a628
Sergey <sergeylisinsky@gmail.com>
2022-10-11 12:07:19 +0300

    add Folder Task010

---
+++ Task010/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int Number = 0;
+Console.WriteLine("Введите 3-х значное число: ");
+Number = Convert.ToInt32(Console.ReadLine()); ;
+if (Number >= 100 && Number <= 999)
+{
+    int DecreaseByTen = Number / 10;
+    int LastDigit = DecreaseByTen % 10;
+    Console.WriteLine("Вторая цифра числа = " + LastDigit);
+}
+else
+{
+    Console.WriteLine("Вы ошиблись, это не 3-х значное число! Попробуйте ещё разок, Вам точно повезёт! ");
+}
