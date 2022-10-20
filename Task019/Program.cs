3a71bcb62122f25bdae98f4701ecff9d5f6df634
Sergey <sergeylisinsky@gmail.com>
2022-10-20 21:38:35 +0300

    add Folder

---
+++ Task019/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int N = Prompt("Введите пятизначное число > ");
+void Palindrome(int N)
+{
+    if (N > 10000 && N < 100000)
+    {
+        int number1 = N / 10000;
+        int number5 = N % 10;
+        if (number1 == number5)
+        {
+            int number2 = N / 1000 % 10;
+            int number4 = N / 10 % 10;
+            if (number2 == number4)
+            {
+                System.Console.WriteLine("Да, Вы совершенно правы - это палиндром");
+            }
+            else
+                System.Console.WriteLine("Нет");
+        }
+        else
+            System.Console.WriteLine("Нет, попробуйте, пожалуйста ещё раз. Я верю, что у Вас всё получится!");
+    }
+    else
+    {
+        System.Console.WriteLine("Это не пятизначное число");
+
+    }
+}
+
+Palindrome(N);
---
+++ Task021/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int Ax = Prompt("Введите координату точки A по оси х > ");
+int Ay = Prompt("Введите координату точки A по оси y > ");
+int Az = Prompt("Введите координату точки A по оси z > ");
+int Bx = Prompt("Введите координату точки B по оси х > ");
+int By = Prompt("Введите координату точки B по оси y > ");
+int Bz = Prompt("Введите координату точки B по оси z > ");
+
+double Length(int x, int y, int z)
+{
+    double result = Math.Sqrt((Ax - Bx) * (Ax - Bx) + (Ay - By) * (Ay - By) + (Az - Bz) * (Az - Bz));
+    System.Console.WriteLine(result);
+    return result;
+}
+Length(Ax, Ay, Az);
