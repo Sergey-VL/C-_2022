b2655d9f87aeb8f26124cd73a9e515e9ec89a5fc
Sergey <sergeylisinsky@gmail.com>
2022-10-21 17:13:53 +0300

    add Folder Task036

---
+++ Task036/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int[] CreatArray(int length)
+{
+    int[] answer = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(0, 100);
+    }
+    return answer;
+}
+
+int SumElements(int[] a)
+{
+    int Sum = 0;
+    for (int i = 1; i < a.Length; i = i + 2)
+    {
+        Sum = Sum + a[i];
+    }
+    return Sum;
+}
+
+void printArray(int[] answer)
+{
+    int count = 0;
+    while (count < answer.Length)
+    {
+        System.Console.Write($"{answer[count]}\t");
+        count++;
+    }
+    System.Console.WriteLine();
+}
+
+int[] array1 = CreatArray(15);
+printArray(array1);
+System.Console.WriteLine(SumElements(array1));
+System.Console.WriteLine();
