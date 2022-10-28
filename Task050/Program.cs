7d36ec069808eab950a7a1cfb1101b50f5c130bb
Sergey <sergeylisinsky@gmail.com>
2022-10-28 19:54:17 +0300

    add Folder Task050

---
+++ Task050/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+for (int i = 0; i < array.GetLowerBound(0); i++)
+{
+    for (int j = 0; j < array.GetUpperBound(0); j++)
+        array[i, j] = Convert.ToInt32(new Random().Next(0, 21));
+}
+
+for (int i = 0; i < array.GetLowerBound(0); i++)
+{
+    for (int j = 0; j < array.GetUpperBound(0); j++)
+        Console.Write(array[i, j] + "\t  ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите значение элемента массива");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > m && b > n)
+    Console.WriteLine("Элемент не существует");
+else
+{
+    object c = array.GetValue(a, b);
+    Console.WriteLine(c);
+}
